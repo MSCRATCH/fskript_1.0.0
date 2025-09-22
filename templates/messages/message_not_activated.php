e22c35e499e942b02da8d19343e1802fb44fff28
MSCRATCH <me@pathologicalplay.com>
2025-09-22 16:32:22 +0200

    Update message_not_activated.php

--- templates/messages/message_not_activated.php
+++ templates/messages/message_not_activated.php
@@ -1,6 +1,7 @@
 <div class="msg_wrapper_mt_centered">
 <span class="msg_span">System message</span>
 <div class="msg_default">
-<p>The user account has not been activated, please contact the administrator.</p>
+<p><?php echo "The user account has not been activated, please contact the administrator";?>.</p>
 </div>
 </div>
+
