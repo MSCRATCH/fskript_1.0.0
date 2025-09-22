9e1c1a981f45efc487772b3a1e7aa250d2a3c964
MSCRATCH <me@pathologicalplay.com>
2025-09-22 16:03:24 +0200

    Add files via upload

---
+++ themes/default_template/footer.php
@@ -0,0 +1,8 @@
+</main>
+<footer class="footer_default">
+<p class="p_mb"><?php echo sanitize(name); ?> <?php echo sanitize(version); ?></p>
+<p class="p_mb">programmed by pathologicalplay</p>
+</footer>
+</div>
+</body>
+</html>
