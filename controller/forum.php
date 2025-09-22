840ff557411460bbe4db99807323da9570b6e4ec
MSCRATCH <me@pathologicalplay.com>
2025-09-22 15:32:06 +0200

    Create forum.php

---
+++ controller/forum.php
@@ -0,0 +1,30 @@
+<?php
+//forum.php [Output of categories.]
+//Forum_controller
+
+class Forum_controller
+{
+
+function __construct($dbh, $header_template, $footer_template, $footer_template_minimal, $settings) {
+$this->dbh = $dbh;
+$this->header_template = $header_template;
+$this->footer_template = $footer_template;
+$this->footer_template_minimal = $footer_template_minimal;
+$this->settings = $settings;
+}
+
+public function index() {
+
+$settings = $this->settings;
+
+require_once $this->header_template;
+
+
+$forum_manager = new ForumManager($this->dbh);
+$rows = $forum_manager->get_all_categories_and_forums();
+include 'templates/frontend_templates/forum_template.php';
+
+require_once $this->footer_template;
+}
+
+}
